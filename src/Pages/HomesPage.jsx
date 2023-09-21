f28efd7ae454f4f8e11005de82847be760ce44be
vgalante2 <vgalante0@gmail.com>
2023-09-21 16:45:31 -0400

    logo fix

--- src/Pages/HomesPage.jsx
+++ src/Pages/HomesPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState} from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
 import { useParams } from 'react-router-dom';
 import homesData from '../components/homes.json';
 import DatePicker from "react-datepicker";
